34bdb9265c2e8fba6e08b9d01c57a9fdf8d10448
ABBA3124 <davideabbadessa73@gmail.com>
2024-07-18 13:12:41 +0200

    Creata NotFoundPage

---
+++ src/components/NotFoundPage/NotFoundPage.jsx
@@ -0,0 +1,12 @@
+import React from "react"
+
+const NotFoundPage = () => {
+  return (
+    <div className="not-found-page">
+      <h1>404 Not Found</h1>
+      <p>La pagina che stai cercando non esiste.</p>
+    </div>
+  )
+}
+
+export default NotFoundPage
