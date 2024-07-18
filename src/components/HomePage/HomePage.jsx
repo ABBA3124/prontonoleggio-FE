d147dd8a67e994e4cbbc2760a6220c51e2e42ebd
ABBA3124 <davideabbadessa73@gmail.com>
2024-07-18 13:24:22 +0200

    Create HomePage

---
+++ src/components/HomePage/HomePage.jsx
@@ -0,0 +1,13 @@
+import React from "react"
+import "./HomePage.css"
+
+const HomePage = () => {
+  return (
+    <div className="homepage">
+      <h1>Benvenuto nella HomePage</h1>
+      <p>Questa è la homepage di ProntoNoleggio.</p>
+    </div>
+  )
+}
+
+export default HomePage
