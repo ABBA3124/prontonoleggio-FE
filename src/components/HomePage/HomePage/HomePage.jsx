6720b39fe14675bdb6142794ca68aad866f3b038
ABBA3124 <davideabbadessa73@gmail.com>
2024-07-20 00:12:36 +0200

    Update HomePage.jsx

--- src/components/HomePage/HomePage/HomePage.jsx
+++ src/components/HomePage/HomePage/HomePage.jsx
@@ -1,11 +1,17 @@
 import React from "react"
 import "./HomePage.css"
 import HeroHomePage from "../Hero/HeroHomePage"
+import FiltraVeicoli from "../FilterVeicoli/FiltraVeicoli"
+import Brand from "../Brand/Brand"
 
 const HomePage = () => {
   return (
     <div className="homepage">
       <HeroHomePage />
+      <FiltraVeicoli />
+      <div className="spazioBrand">
+        <Brand />
+      </div>
     </div>
   )
 }
