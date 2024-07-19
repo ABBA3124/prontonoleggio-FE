74a6f3b11a1c49ee991e0a94f74b74c1ff4353c4
ABBA3124 <davideabbadessa73@gmail.com>
2024-07-19 16:33:58 +0200

    Create HomePage.jsx

---
+++ src/components/HomePage/HomePage/HomePage.jsx
@@ -0,0 +1,13 @@
+import React from "react"
+import "./HomePage.css"
+import HeroHomePage from "../Hero/HeroHomePage"
+
+const HomePage = () => {
+  return (
+    <div className="homepage">
+      <HeroHomePage />
+    </div>
+  )
+}
+
+export default HomePage
